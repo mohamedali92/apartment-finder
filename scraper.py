09c1f90feee111a8f97e1ce8b20c13c6ce2e7335
Mohamed Ali <mohamed.ali@alumni.ubc.ca>
2017-07-14 22:47:40 -0700

    added email notification functionality

--- scraper.py
+++ scraper.py
@@ -1,24 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+import yagmail
 
 
-# Email Settings
-
-smtp_server = 'email-smtp.us-west-2.amazonaws.com'
-smtp_username = 'AKIAJOHYW36LMJJHE4RA'
-smtp_password = 'Aj0uTg5fxjYW3wIRm3QhP37KElOjAyGaN+45Dyv8mivq'
-smtp_port = '587'
-smtp_do_tls = True
-
-s = smtplib.SMTP(host=smtp_server,port=smtp_port,timeout=10)
-s.starttls()
-s.ehlo()
-s.login(smtp_username, smtp_password)
 # Scraper Settings
 area = "vancouver"
 subArea = "rch"
 baseUrl = "https://" + area + ".craigslist.ca"
 
+#users
+users = [{"name": "Mohamed", "email": "mohamed.ali@alumni.ubc.ca"}]
+yag = yagmail.SMTP('mohammedali.saisdubai@gmail.com', '16julyfriday')
+
+
+def generateHtmlForListing(listings):
+    htmlString = ''
+    for listing in listings:
+        htmlString += '<h1>' + listing["title"] + '</h1>'
+        htmlString += '<a href="' + listing["url"] + '"> Check out listing!</a>'
+    return htmlString
+
+def sendEmails(users, listings):
+    htmlForListings = generateHtmlForListing(listings)
+    userEmails = [user["email"] for user in users]
+    yag.send(to = userEmails, subject = 'New Listings Found', contents = [htmlForListings])
+    
 result = requests.get(
     baseUrl + "/search/" + subArea + "/apa?hasPic=1&postedToday=1&searchNearby=1&min_bedrooms=3&max_bedrooms=3&availabilityMode=0")
 if result.status_code == 200:
@@ -31,6 +37,8 @@ if result.status_code == 200:
         listingUrl = baseUrl + listingContent.get('href')
         listings.append({"title": listingTitle, "url": listingUrl})
     print (listings)
+    print(generateHtmlForListing(listings))
+    
+    if len(listings) > 0:
+        sendEmails(users, listings)
 
-
-def sendEmail():
